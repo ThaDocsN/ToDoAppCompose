8545f99f3972ab20ef7c4b8f54d666e4be78b9a3
ThaDocsN <charles.tha.doc@gamil.com>
2021-11-05 10:23:14 -0500

    add viewModel

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -3,21 +3,27 @@ package com.example.todoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.todoapp.ui.theme.ToDoAppTheme
+import com.example.todoapp.viewmodels.ToDoViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<ToDoViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ToDoAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
+                    // TODO: 11/5/21  
                 }
             }
         }
@@ -25,14 +31,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TodoActivityScreen(viewModel: ToDoViewModel) {
+    // TODO: 11/5/21  
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ToDoAppTheme {
-        Greeting("Android")
+        // TODO: 11/5/21  
     }
 }
---
+++ app/src/main/java/com/example/todoapp/viewmodels/ToDoViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.todoapp.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.todoapp.ToDoItem
+
+class ToDoViewModel: ViewModel() {
+    private val _todoItems = MutableLiveData(listOf<ToDoItem>())
+    val todoItems : LiveData<List<ToDoItem>> = _todoItems
+
+    fun addItem(item: ToDoItem){
+        // TODO: 11/5/21  
+    }
+
+    fun removeItem(item: ToDoItem) {
+        // TODO: 11/5/21  
+    }
+}
